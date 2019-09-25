0a12d0cd18b72725f54ed8b18fd9e5eee49449fb
Asus <Asus@DESKTOP-71JSGHS>
2019-09-25 18:36:03 +0100

    Second version

--- src/prac3/Lienzo.java
+++ src/prac3/Lienzo.java
@@ -1,19 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package prac3;
 
-
 import java.awt.Color;
+import static java.awt.Color.BLACK;
+import static java.awt.Color.WHITE;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import javax.swing.JPanel;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -23,8 +23,11 @@ public class Lienzo extends JPanel{
     private Color colorFondo;
     private Color colorPincel;
     private float grosor;
-    
     ArrayList<Point2D> myPoints = new ArrayList<Point2D>();
+    public Lienzo(){
+        colorFondo = WHITE;
+        colorPincel = BLACK;
+    }
     
     @Override 
     public void paintComponent(Graphics g){
@@ -38,15 +41,18 @@ public class Lienzo extends JPanel{
         int py[] = new int [myPoints.size()];
         
         for (int i=0; i< myPoints.size();i++){
-            //g.drawOval(i,i,WIDTH,HEIGHT)
+            
             px[i] = (int)myPoints.get(i).getX();
             py[i] = (int)myPoints.get(i).getY();
+            g.drawOval(px[i],py[i],(int)grosor,(int)grosor);
         }
-        //g2d.drawLine(px,py,myPoints.size());
+        //g2d.drawOval(px,py,grosor,grosor);
     }
     
     public void pinta(Point punto, int valor){
         myPoints.add(punto);
+        if(myPoints.size() > 10) myPoints.remove(0);
+        
     }
     
     public void setColorFondo(Color color){
@@ -57,6 +63,6 @@ public class Lienzo extends JPanel{
     }
     public void setGrosor(float nGrosor){
         grosor = nGrosor;
+        
     }
-    
 }
